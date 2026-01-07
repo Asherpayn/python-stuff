1695e85225a86720d1f94530fe4a735ee0b292d4
Asher payn <asherpayn@gmail.com>
2026-01-07 18:56:25 +0000

    Finished, gave up on weather api and i opted for Y/N question.

--- RioTheParrot.py
+++ RioTheParrot.py
@@ -1,20 +1,31 @@
 # Determining Rio the parrot's talkativeness.
 # the parrot is talkative if: it's between 5-10AM and/or it is sunny outside.
+# AI: I did not use ai to write any of this code. There would be a CLAUDE.md file, or it would be in .gitignore
+# It might have been used to simplify/find python documentation as it sometimes looks like another language (not just the python snippets)
 
 from datetime import datetime
 
-# Things I need to do:
+# Tasks:
 # - get current time (DONE)
-# - get current weather
-# - figure Out whether Rio is talkative
+# - get current weather (DONE) - giving up on actual data I will instead ask you to look out the window
+# - figure Out whether Rio is talkative (DONE)
 
-# this is a hardcoded time for while I find out how to get the current time. (will remove)
-fake_time = 5
+# this is a hardcoded time (will remove)
+fake_time = 6
 
 # this gets and prints the actual time
 now = datetime.now()
+print(now)
 hour = now.hour
-# print(now)
-# print(hour)
 
 print(f"The current time is: {hour}")
+
+# Now getting the weather
+weatherquestion = input("Look out the window, is it sunny? Y/N: ")
+is_sunny = weatherquestion == "Y"
+
+# Is Rio going to be talkative?
+if is_sunny or (5 <= hour <= 10):
+    print("Rio The Parrot will be talkative")
+else:
+    print("Rio The Parrot will not be talkative")
