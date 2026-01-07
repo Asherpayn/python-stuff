fcb32042760ebb0e2563f3de03dc8b99422cd233
Asher payn <asherpayn@gmail.com>
2026-01-07 17:31:28 +0000

    added Jetbrains configs and updated the parrot file with some small changes and typo fixes.

--- RioTheParrot.py
+++ RioTheParrot.py
@@ -1,21 +1,20 @@
 # Determining Rio the parrot's talkativeness.
-# the parrot is talkative if: its between 5-10AM and/or it is sunny outside.
+# the parrot is talkative if: it's between 5-10AM and/or it is sunny outside.
 
 from datetime import datetime
 
-# Things i need to do:
-# - get current time
+# Things I need to do:
+# - get current time (DONE)
 # - get current weather
 # - figure Out whether Rio is talkative
 
-# this is a harcoded time for while i find out how to get the current time.
-current_time = 5
+# this is a hardcoded time for while I find out how to get the current time. (will remove)
+fake_time = 5
 
-# his prints the actual time, this will be used later
+# this gets and prints the actual time
 now = datetime.now()
 hour = now.hour
-print(now)
-print(hour)
+# print(now)
+# print(hour)
 
-
-print(f"The current time is: {current_time} AM/PM")
+print(f"The current time is: {hour}")
