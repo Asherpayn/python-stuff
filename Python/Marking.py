77259b71605b0abfe059e8398e5f871936d820b0
Asher Payn <asherpayn@gmail.com>
2026-01-12 12:56:00 +0000

    Moved existing files to a folder so it is easier to manage when looking for stuff

---
+++ Python/Marking.py
@@ -0,0 +1,7 @@
+# This is a bit of python to figure how well you did in a test
+# It should ask for your score and how much it is out of then give you a grade
+
+# Getting the score
+score = input("Enter your number of marks: ")
+out_of = input("Enter the total possible marks: ")
+
