73bec0a13a1c46ef94aea9647a94441559dc9a4d
Asher Payn <asherpayn@gmail.com>
2026-03-02 12:44:40 +0000

    added new line

--- stringsworksheet.py
+++ stringsworksheet.py
@@ -15,6 +15,7 @@ print ("Your surname is,", surname)
 print ("Hello,", firstname, surname)
 print("Your initials are:", firstname[0], surname[0])
 print(firstname.upper() + " " + surname.upper())
+
 # saving fullname - the " " adds space between the variables
 fullname = firstname + " " + surname
 print(fullname)
